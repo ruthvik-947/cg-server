7c19e6287eb084f7db1d1165043f58b4327d9da2
Ruthvik Peddawandla <118911359+ruthvik-947@users.noreply.github.com>
2023-03-25 15:08:42 +0000

    Add OpenAI base code, define read endpoint

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+import openai
 
 app = Flask(__name__)
+
+api_key = "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"
+model_engine = "gpt-3.5-turbo"
+engine = None
+openai.api_key = api_key
+engine = openai.Model.get(model_engine)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+  return
+
+
+@app.route("/api/read", methods=["POST"])
+def read():
+  data = request.get_json()
+  exercise_prompt = "Summarise this in Italian: "
+  text = exercise_prompt + data["text"]
+
+  completions = engine.complete(prompt=text, max_tokens=5)
+  message = completions.choices[0].text
+
+  return message
 
 
 app.run(host='0.0.0.0', port=81)
