5eb65e68f24bb8c28bd038532ac4da0d8ea419a5
Ruthvik Peddawandla <118911359+ruthvik-947@users.noreply.github.com>
2023-03-26 10:57:41 +0000

    Server functional

---
+++ config.py
@@ -0,0 +1,1 @@
+OPENAI_API_KEY = "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"
--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from flask import Flask, request
-import secrets
 import openai
+import config
 
 app = Flask(__name__)
 
-api_key = secrets.OPENAI_API_KEY
-model_engine = "gpt-3.5-turbo"
-engine = None
-openai.api_key = api_key
-engine = openai.Model.get(model_engine)
+model = 'davinci'
+openai.api_key = config.OPENAI_API_KEY
 
 
 @app.route('/')
 def index():
-  return
+  return "ChatGepetto is sentient."
 
 
 @app.route("/api/read", methods=["POST"])
@@ -22,7 +19,12 @@ def read():
   exercise_prompt = "Summarise this in Italian: "
   text = exercise_prompt + data["content"]
 
-  completions = engine.complete(prompt=text, max_tokens=5)
+  completions = openai.Completion.create(model=model,
+                                         prompt=text,
+                                         max_tokens=7,
+                                         temperature=0)
+
+  print(completions)
   message = completions.choices[0].text
 
   return message
